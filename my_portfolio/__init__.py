1b938b69a70f204680a91d833eb63c57a35c3b69
Dikachis <dikachi015safety@gmail.com>
2022-12-02 18:56:33 +0100

    Updated file

--- my_portfolio/__init__.py
+++ my_portfolio/__init__.py
@@ -3,6 +3,12 @@ from flask import Flask
 # to create flask application
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'how are you'
+    app.config['SECRET_KEY'] = 'hcfgdhgfj ghdgdgdg'
+    
+    from .templates.views import views
+    from .templates.auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
