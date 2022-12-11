79f8adba294022b58dba7d265b58a319f4ea0eb3
Dikachis <dikachi015safety@gmail.com>
2022-12-11 12:51:41 +0100

    Updated files

--- my_portfolio/static/js/script.js
+++ my_portfolio/static/js/script.js
@@ -34,3 +34,17 @@ document.querySelectorAll('a').forEach(links =>{
     }
 
 });
+
+// landing page starts
+window.onscroll = function() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 ) {
+        document.getElementById('navbar').classList.add('scrolled');
+    } else {
+        document.getElementById('navbar').classList.remove('scrolled');
+    }
+}
+
+// AOS
+AOS.init({
+    duration: 800,
+});
